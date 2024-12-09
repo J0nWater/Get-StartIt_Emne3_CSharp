884bfa7dd1595654a1a05e9711767d01a8411277
J0nWater <79053067+J0nWater@users.noreply.github.com>
2024-12-09 20:44:39 +0100

    Add project

---
+++ 8_Oppgave3_If_Else/Program.cs
@@ -0,0 +1,29 @@
+﻿//Oppgave 3: Lag en metode som tar inn to int verdier.
+//Dersom en av de, eller summen til int verdiene blir 30,
+//skal metoden returnere true. Ellers returnerer metoden false
+
+Console.WriteLine("Checking if one of the numbers or the sum is equal to 30\n");
+
+Console.WriteLine("Enter the first number");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the second number");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int sum = number1 + number2;
+bool answer = false;
+
+CheckNumbers();
+void CheckNumbers()
+{
+    if (number1 == 30 || number2 == 30 || sum == 30)
+    {
+        answer = true;
+        Console.WriteLine($"\nThe answer is: {answer} - Number 30 was found");
+    }
+    else
+    {
+        answer = false;
+        Console.WriteLine($"\nThe answer is: {answer} - Number 30 is not found");
+    }
+}
