6302df76af66b1b6d950a1ef639d770778a4967d
J0nWater <79053067+J0nWater@users.noreply.github.com>
2024-12-13 14:07:52 +0100

    Add Project

---
+++ 9_Oppgave-RandomHobbygenerator/Program.cs
@@ -0,0 +1,4 @@
+﻿using _9_Oppgave_RandomHobbygenerator;
+
+App app = new App();
+app.Run();
---
+++ 9_Oppgave-RandomHobbygenerator/RandomHobbyGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_Oppgave_RandomHobbygenerator
+{
+    internal class App
+    {
+        string userName;
+        public void Run()
+        {
+            Console.WriteLine("Who would like a new hobby?");
+            userName = Console.ReadLine();
+            GetRandomHobby();
+        }
+        void GetRandomHobby()
+        {
+            Random rand = new Random();
+            var randomHobby = rand.Next(0, 4);
+            if (randomHobby == 0)
+            {
+                Console.WriteLine($"{userName} is now an amateur astronomer. Eyes on the skies!\n");
+                Run();
+            }
+            else if (randomHobby == 1)
+            {
+                Console.WriteLine($"{userName} is now a classic literature enthusiast. Lost in the pages.\n");
+                Run();
+            }
+            else if (randomHobby == 2)
+            {
+                Console.WriteLine($"{userName} is now a classical pianist. Creating melodies for the soul.\n");
+                Run();
+            }
+            else if (randomHobby == 3)
+            {
+                Console.WriteLine($"{userName} is now a marathon runner. Breaking personal records!\n");
+                Run();
+            }
+        }
+    }
+}
