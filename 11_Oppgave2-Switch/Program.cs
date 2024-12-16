42cc4fbc2c9f7618829720fadfe6774eb7b2917e
J0nWater <79053067+J0nWater@users.noreply.github.com>
2024-12-16 14:20:42 +0100

    Add project

---
+++ 11_Oppgave2-Switch/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+Oppgave 2: 
+
+Ta utgangspunkt fra oppgave 1, men istedenfor å bruke en int med en fastsatt value, 
+bruker du Console.ReadLine() for at man kan skrive inn et tall i konsollen. 
+Hvis man ikke får inn et tall, skal switch-en kjøre programmet på nytt.
+ */
+
+using _11_Oppgave2_Switch;
+
+Week app = new Week();
+app.Run();
+
---
+++ 11_Oppgave2-Switch/Week.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_Oppgave2_Switch
+{
+    internal class Week
+    {
+        public int dayInput;
+        public void Run()
+        {
+            Console.WriteLine("Give a value from 1 to 7 to find out what day of the week it is");
+            dayInput = Convert.ToInt32(Console.ReadLine());
+            FindDay();
+        }
+        void FindDay()
+        {
+            switch (dayInput)
+            {
+                case 1:
+                    Console.WriteLine("it's Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("it's Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("it's Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("it's Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("it's Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("it's Saturday");
+                    break;
+                case 7:
+                    Console.WriteLine("it's Sunday");
+                    break;
+                default:
+                    Console.WriteLine("Try again!");
+                    Run();
+                    break;
+            }
+        }
+    }
+}
